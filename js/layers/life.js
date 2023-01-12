44f68a6edcaec4b9a46922712e41588a23f0f6a6
Yahkub7 <Yahkub@gmail.com>
2023-01-11 19:58:26 -0800

    asd

--- js/layers/life.js
+++ js/layers/life.js
@@ -4,12 +4,11 @@ addLayer("l", {
     startData() { return {
         unlocked: true,
 		points: new Decimal(50),
-        salary: [0,3],
-        drain: [0,1],
-        xp: [0,1],
         job: 0,
-        hired: false,
-        clickables: {[11]: "Quit"},
+        earn: 0,
+        drain: 0,
+        xp: 0,
+        rec: 0,
         gridy: 101,
     }},
     row: 0,
@@ -41,6 +40,7 @@ addLayer("l", {
                 "blank",
                 ["display-text", function() {return player.l.gridy}],
                 ["bar","staBar"],
+                ["display-text", function() {if (player.l.rest) return `<b style="color: #ff6961">Exhausted:</b> Recovering Slowly`}],
                 "grid",
                 "blank",
                 ["microtabs","life"],
@@ -65,8 +65,13 @@ addLayer("l", {
         life: {
             work: {
                 content: [
+                    ["display-text", function() {
+                        dis = player.l.job
+                        if (player.l.job==0) dis = "Clearing"
+                        return "Currently: "+dis
+                    }],
                     "blank",
-                    ["clickable",11],
+                    "clickables",
                 ]
             },
         },
@@ -80,118 +85,156 @@ addLayer("l", {
             baseStyle: {'background-color' : "#696969"},
             textStyle: {'color': '#04e050'},
             progress() { return player.st.sta.div(player.st.con.mul(2).minus(10)) },
-            display() { return Math.round(player.st.sta)+" / "+player.st.con.mul(2).minus(10)},
+            display() { return Math.floor(player.st.sta)+" / "+player.st.con.mul(2).minus(10)},
         },
     },
     grid: {
         rows: 1,
         cols: 10,
-        getStartData(id) {
-          return 100
-        },
+        maxrows: 1,
+        maxcols: 10,
+        getStartData(id) { return {
+            job:0,
+            earn:0,
+            drain:0,
+            xp:0,
+            rec:0,
+        }},
         getCanClick(data, id) {
-          return false
+          return true
         },
         onClick(data, id) {
-
+            setGridData(this.layer,id,{
+                job:player.l.job,
+                earn:player.l.earn,
+                drain:player.l.drain,
+                xp:player.l.xp,
+                rec:player.l.rec,
+            })
         },
         getDisplay(data, id) {
-          return data
+          if (data.job==0) return "<br>unassigned"
+          let act = "<br>"
+          if (player.l.gridy==id) act = "*<br>"
+          return act+data.job
         },
         getUnlocked(id) {
           return true
         }
     },
+    skipBS() {
+        while (player.l.grid[player.l.gridy].job==0&&player.l.gridy<111) player.l.gridy++
+        if (player.l.gridy>110) player.l.gridy = 101
+    },
+    doAction() {
+        let id = player.l.gridy
+        
+        let earn = player.l.grid[id].earn
+        if (earn == undefined) earn = 0
+    
+        let drain = player.l.grid[id].drain
+        if (drain == undefined) drain = 0
+    
+        let xp = player.l.grid[id].xp
+        if (xp == undefined) xp = 0
+    
+        let rec = player.l.grid[id].rec
+        if (rec == undefined) rec = 0
+    
+        let max = player.st.con.mul(2).minus(10)
+        
+        if (player.l.resetTime>=1) {
+            player.l.resetTime = 0
+            console.log("reset time")
+            if (!player.l.rest&&player.st.sta.minus(drain).gte(0)) {
+                addPoints('l',earn)
+                player.st.xp = player.st.xp.add(xp)
+                player.st.sta = player.st.sta.minus(drain)
+                if (player.st.sta.add(rec).gt(max)) player.st.sta = max
+                if (player.st.sta.add(rec).lte(max)) player.st.sta = player.st.sta.add(rec)
+                player.l.gridy++
+                console.log("did action "+id)
+                console.log("earn: "+earn)
+                console.log("drain: "+drain)
+                console.log("xp: "+xp)
+                console.log("rec: "+rec)
+            }
+            if (!player.l.rest&&player.st.sta.minus(drain).lt(0)) { 
+                player.st.sta = player.st.sta.minus(player.st.sta)
+                player.l.rest = true 
+                console.log("got exhausted")
+            }
+            if (player.l.rest) {
+                if (player.st.sta.add(player.st.con.div(20)).gt(max)) player.st.sta = max
+                if (player.st.sta.add(player.st.con.div(20)).lte(max)) player.st.sta = player.st.sta.add(player.st.con.div(20))
+                console.log("exhausted sta gain")
+                if (player.st.sta.gte(max)) { player.l.rest = false
+                console.log("finished exhaustion")
+                }
+            }
+            
+        }
+    },
     clickables: {
         11: {
-            title: "Stable Hand",
+            title: "Clear",
             display() { 
-                let data = getClickableState(this.layer, this.id)
-                let sal = '<br>'+format(player.l.salary[1])+' gold/tick'
-                return "click to " + data + sal
+                let a = "click to assign<br>"
+                return a
             },
             onClick() { 
-                switch(getClickableState(this.layer, this.id)){
-                    case "Work":
-                        player.l.hired = true
-                        player.l.job = 1
-                        player[this.layer].clickables[this.id] = "Quit"
-                        break;
-                    case "Quit":
-                        player.l.hired = false
-                        player.l.job = 0
-                        player[this.layer].clickables[this.id] = "Work"
-                        break;                
-            }},
+                player.l.job = 0
+            },
             canClick() {
-                switch(getClickableState(this.layer, this.id)){
-                    case "Work":
-                        return !player.l.hired
-                    case "Quit":
-                        return true
-            }},
+                return true
+            },
             style() {
-                switch(getClickableState(this.layer, this.id)){
-                    case "Work":
-                        let dis = {'background-color': 'red'}
-                        if (player.l.hired) dis = {'background-color':'gray'}
-                        return dis
-                    case "Quit":
-                        return {'background-color': 'green'}
-            }},
+                
+            },
+        },
+        12: {
+            title: "Stable Hand",
+            display() { 
+                let a = "click to assign<br>"
+                let b = "Gold: 3<br>"
+                let c = "Stam: -1<br>"
+                let d = "Xp: 1<br>"
+                return a+b+c+d
+            },
+            onClick() { 
+                player.l.job = "Stable Hand"
+                player.l.earn = 3
+                player.l.drain = 1
+                player.l.xp = 1
+                player.l.rec = 0
+            },
+            canClick() {
+                return true
+            },
+            style() {
+                
+            },
+        },
+        31: {
+            title: "Rest",
+            display() { 
+                let a = "click to assign<br>"
+                let b = "gain 2 sta"
+                return a+b
+            },
+            onClick() { 
+                player.l.job = "Rest"
+                player.l.earn = 0
+                player.l.drain = 0
+                player.l.xp = 0
+                player.l.rec = 2
+            },
+            canClick() {
+                return true
+            },
+            style() {
+                
+            },
         },
     },
 })
-setInterval(function() {
-    let id = player.l.gridy
-    let earn = player.l.grid[id].earn
-    let drain = player.l.grid[id].drain
-    let xp = player.l.grid[id].xp
-    let max = player.st.con.mul(2).minus(10)
-    let rec = max.div(10)
-
-    if (!player.l.rest&&player.st.sta.minus(drain).gte(0)) {
-        addPoints('l',earn)
-        player.st.xp = player.st.xp.add(xp)
-        player.st.sta = player.st.sta.minus(drain)
-        player.l.gridy = player.l.gridy+1
-        
-    }
-    else { player.l.rest = true }
-    if (player.l.rest) {
-        if (player.st.sta.add(rec).gt(max)) player.st.sta = max
-        if (player.st.sta.add(rec).lte(max)) player.st.sta = player.st.sta.add(rec)
-    }
-    if (player.st.sta.gte(max)) {
-        player.l.rest = false
-    }
-    if (player.l.gridy>=111) player.l.gridy = 101
-
-},1000)
-
-/*setInterval(function() {
-    let drain = player.l.drain[player.l.job]
-    
-    let earn = player.l.salary[player.l.job]
-
-    let exp = player.l.xp[player.l.job]
-    
-    let max = player.st.con.mul(2).minus(10)
-
-    let rec = max.div(10)
-
-    if (!player.l.hired) player.l.working = false
-    if (player.st.sta.minus(drain).lt(0)) player.l.working = false
-    if (player.l.hired) {
-        if (player.st.sta.gte(max)) player.l.working = true
-    }
-    if (player.l.working&&player.st.sta.minus(drain).gte(0)) {
-        addPoints('l',earn)
-        player.st.xp = player.st.xp.add(exp)
-        player.st.sta = player.st.sta.minus(drain)
-    }
-    if (player.l.working==false&&player.st.sta.lt(max)) {
-        if (player.st.sta.add(rec).gt(max)) player.st.sta = max
-        if (player.st.sta.add(rec).lte(max)) player.st.sta = player.st.sta.add(rec)
-    }
-}, 1000)*/
